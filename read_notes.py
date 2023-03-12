fff9470655e5ddf590249318104959d829d5cf82
Boris Shapovalov <bashap@bk.ru>
2023-03-12 23:16:26 +0300

    Модуль чтения

---
+++ read_notes.py
@@ -0,0 +1,14 @@
+import csv
+from datetime import datetime
+
+# чтение заметок из файла
+
+def read_notes():
+    notes = []
+    with open('notes.csv','r') as f:
+        reader = csv.DictReader(f, delimiter=',')
+        for row in reader:
+            row['id'] = int(row['id'])
+            notes.append(row)
+    return notes
+
