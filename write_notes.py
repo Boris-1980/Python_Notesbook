6859b149cb327650869364670db370434ee8bffe
Boris Shapovalov <bashap@bk.ru>
2023-03-12 23:17:12 +0300

    модуль записи

---
+++ write_notes.py
@@ -0,0 +1,12 @@
+import csv
+from datetime import datetime
+
+# запись заметок в файл
+
+def write_notes(notes):
+    with open('notes.csv','w',newline='') as f:
+        writer = csv.DictWriter(f,fieldnames=['id','title','body','created_at','update_at'], delimiter=',')
+        writer.writeheader()
+        for note in notes:
+            writer.writerow(note)
+
